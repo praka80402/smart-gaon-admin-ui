d161b53217769510f283425e7d41b53d0095773c
Prakash Kumar <prakashpandeyec032@gmail.com>
2026-01-01 12:41:42 +0530

    login issue fixed

--- src/pages/userService.js
+++ src/pages/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = "http://smartgaonadmin.duckdns.org/admin";
+const BASE = "https://smartgaonadmin.duckdns.org/admin";
 const LOGIN_URL = `${BASE}/login`;
 const USERS_URL = `${BASE}/users`;
 const ADMINS_URL = `${BASE}/all`;
