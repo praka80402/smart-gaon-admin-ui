3ce20794a59851d88d4e75292af2aa1feeadd4c0
Prakash Kumar <prakashpandeyec032@gmail.com>
2026-01-02 21:17:54 +0530

    issue fixed

--- src/pages/donation/services/axiosInstance.js
+++ src/pages/donation/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:9090/api",
+  baseURL: "https://smartgaonadmin.duckdns.org/admin/api",
   headers: {
     "Content-Type": "application/json",
   },
