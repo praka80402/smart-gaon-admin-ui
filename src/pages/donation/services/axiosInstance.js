a66b6f2ad9e4a60ec1099b113df1774b3e025ae6
Prakash Kumar <prakashpandeyec032@gmail.com>
2026-01-06 11:31:28 +0530

    dev deployment

--- src/pages/donation/services/axiosInstance.js
+++ src/pages/donation/services/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-//   baseURL: "https://smartgaonadmin.duckdns.org/api",
-baseURL:"http://localhost:9090/api",
+     baseURL: "https://smartgaonadmin.duckdns.org/api",
+//baseURL:"http://localhost:9090/api",
   // headers: {
   //   "Content-Type": "application/json",
   // },
--- src/pages/gaonconnect/services/apiConfig.js
+++ src/pages/gaonconnect/services/apiConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    // baseURL: "https://smartgaonadmin.duckdns.org",
-     baseURL: "http://localhost:9090",
+    baseURL: "https://smartgaonadmin.duckdns.org",
+    // baseURL: "http://localhost:9090",
 });
 
 // api.interceptors.request.use((config) => {
