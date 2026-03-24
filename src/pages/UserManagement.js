3d2a31c74da61305bd24aa5695dd0c0afdc15b9f
Prakash Kumar <prakashpandeyec032@gmail.com>
2026-03-24 11:44:22 +0530

    changes

--- src/pages/UserManagement.js
+++ src/pages/UserManagement.js
@@ -518,7 +518,7 @@ setAdminForm({ ...adminForm, pincode: e.target.value })
       <option value="Bihar">Bihar</option>
       <option value="UP">UP</option>
       <option value="Maharashtra">Maharashtra</option>
-       <option value="Madhya Pardesh">Madhya Pardesh</option>
+       <option value="Madhya Pradesh">Madhya Pradesh</option>
       <option value="Jharkhand">Jharkhand</option>
       <option value="Gujarat">Gujarat</option>
     </select>
